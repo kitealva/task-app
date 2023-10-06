75d5ff2e9e0f50e154d27dabac5672c2057a80a3
Brady <kite.alva@gmail.com>
2023-10-06 11:27:42 -0600

    first-commit-

---
+++ src/app/pages/_app.tsx
@@ -0,0 +1,16 @@
+import '@mantine/core/styles.css';
+
+import type { AppProps } from 'next/app';
+import { MantineProvider, createTheme, } from '@mantine/core';
+
+const theme = createTheme({
+
+});
+
+export default function App({ Component, pageProps }: AppProps) {
+  return (
+    <MantineProvider theme={theme}>
+      <Component {...pageProps} />
+    </MantineProvider>
+  );
+}
---
+++ src/app/test2/page.tsx
@@ -0,0 +1,9 @@
+const Test = () => {
+  return (
+    <div>tes</div>
+
+
+  )
+}
+
+export default Test
